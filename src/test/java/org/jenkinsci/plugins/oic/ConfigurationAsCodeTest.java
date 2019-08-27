be1f91a513b5699c8a02197cdcfa39de2e71e5ef
Zhao Xiaojie <LinuxSuRen@gmail.com>
2019-08-27 11:37:11 +0800

    Add more test cases

--- src/test/java/org/jenkinsci/plugins/oic/ConfigurationAsCodeTest.java
+++ src/test/java/org/jenkinsci/plugins/oic/ConfigurationAsCodeTest.java
@@ -1,12 +1,21 @@
 package org.jenkinsci.plugins.oic;
 
 import hudson.security.SecurityRealm;
+import io.jenkins.plugins.casc.ConfigurationContext;
+import io.jenkins.plugins.casc.ConfiguratorRegistry;
 import io.jenkins.plugins.casc.misc.ConfiguredWithCode;
 import io.jenkins.plugins.casc.misc.JenkinsConfiguredWithCodeRule;
+import io.jenkins.plugins.casc.model.CNode;
 import org.junit.ClassRule;
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import static io.jenkins.plugins.casc.misc.Util.*;
+import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
 import static org.junit.Assert.assertTrue;
 
 public class ConfigurationAsCodeTest {
@@ -36,5 +45,26 @@ public class ConfigurationAsCodeTest {
         assertEquals("scopes", oicSecurityRealm.getScopes());
         assertEquals("http://localhost", oicSecurityRealm.getTokenServerUrl());
         assertEquals("userNameField", oicSecurityRealm.getUserNameField());
+    }
+
+    @Test
+    public void testExport() throws Exception {
+        ConfigurationContext context = new ConfigurationContext(ConfiguratorRegistry.get());
+        CNode yourAttribute = getJenkinsRoot(context).get("securityRealm").asMapping().get("oic");
+
+        String exported = toYamlString(yourAttribute);
+
+        // secrets are always changing. so, just remove them before there's a better solution
+        String[] lines = exported.split("\n");
+        List<String> lineList = new ArrayList<>();
+        for(String line : lines) {
+            if(!line.contains("Secret")) {
+                lineList.add(line);
+            }
+        }
+        String cleanedExported = String.join("\n", lineList);
+        String expected = toStringFromYamlFile(this, "ConfigurationAsCodeExport.yml");
+
+        assertThat(cleanedExported, is(expected));
     }
 }
