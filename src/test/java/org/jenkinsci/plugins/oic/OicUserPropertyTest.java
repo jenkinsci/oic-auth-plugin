35b125ae6a2f8b2f449d698d3a06003c744c3cfd
Derek Shirley <derek.shirley@ised-isde.gc.ca>
2022-05-09 16:41:47 -0400

    testGetAuthorities test

--- src/test/java/org/jenkinsci/plugins/oic/OicUserPropertyTest.java
+++ src/test/java/org/jenkinsci/plugins/oic/OicUserPropertyTest.java
@@ -1,5 +1,7 @@
 package org.jenkinsci.plugins.oic;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.containsInAnyOrder;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
@@ -9,16 +11,20 @@ import org.junit.Test;
 
 public class OicUserPropertyTest {
 
-    private static final GrantedAuthorityImpl AUTH1 = new GrantedAuthorityImpl("admin");
+    public static final String ADMIN = "admin";
 
-    private static final GrantedAuthorityImpl AUTH2 = new GrantedAuthorityImpl("admin");
+    public static final String READ = "read";
+
+    private static final GrantedAuthorityImpl GRANTED_AUTH1 = new GrantedAuthorityImpl(ADMIN);
+
+    private static final GrantedAuthorityImpl GRANTED_AUTH2 = new GrantedAuthorityImpl(READ);
 
     private OicUserProperty userProp;
 
     @Test
     public void testGetAllGrantedAuthorities() {
         String userName = "derek";
-        GrantedAuthority[] authorities = new GrantedAuthority[]{AUTH1, AUTH2};
+        GrantedAuthority[] authorities = new GrantedAuthority[]{GRANTED_AUTH1, GRANTED_AUTH2};
         userProp = new OicUserProperty(userName, authorities);
 
         StringBuilder result = new StringBuilder();
@@ -33,9 +39,18 @@ public class OicUserPropertyTest {
     }
 
     @Test
+    public void testGetAuthorities() {
+        String userName = "derek";
+        GrantedAuthority[] authorities = new GrantedAuthority[]{GRANTED_AUTH1, GRANTED_AUTH2};
+        userProp = new OicUserProperty(userName, authorities);
+
+        assertThat(userProp.getAuthorities(), containsInAnyOrder(READ, ADMIN));
+    }
+
+    @Test
     public void testGetDescriptor() {
         String userName = "derek";
-        GrantedAuthority[] authorities = new GrantedAuthority[]{AUTH1, AUTH2};
+        GrantedAuthority[] authorities = new GrantedAuthority[]{GRANTED_AUTH1, GRANTED_AUTH2};
         userProp = new OicUserProperty(userName, authorities);
         assertNotNull(userProp.getDescriptor());
     }
