00e62a4c5cf2911927da68f8a71438322975dc97
Derek Shirley <derek.shirley@ised-isde.gc.ca>
2022-05-11 11:22:46 -0400

    more tests

---
+++ src/test/java/org/jenkinsci/plugins/oic/JenkinsAwareConnectionFactoryTest.java
@@ -0,0 +1,39 @@
+package org.jenkinsci.plugins.oic;
+
+import static org.junit.Assert.assertNotNull;
+
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.jvnet.hudson.test.JenkinsRule;
+
+import hudson.ProxyConfiguration;
+import jenkins.model.Jenkins;
+
+public class JenkinsAwareConnectionFactoryTest {
+
+    private JenkinsAwareConnectionFactory factory = new JenkinsAwareConnectionFactory();
+
+    @Rule
+    public JenkinsRule jenkinsRule = new JenkinsRule();
+
+    @Test
+    public void testOpenConnection_WithNullProxy() throws ClassCastException, IOException {
+        Jenkins.getInstance().proxy = null;
+        URL url = new URL("http://localhost");
+        HttpURLConnection conn = factory.openConnection(url);
+        assertNotNull(conn);
+    }
+
+    @Test
+    public void testOpenConnection_WithProxy() throws ClassCastException, IOException {
+        Jenkins.getInstance().proxy = new ProxyConfiguration("someHost", 8000);
+        URL url = new URL("http://localhost");
+        HttpURLConnection conn = factory.openConnection(url);
+        assertNotNull(conn);
+    }
+}
+
