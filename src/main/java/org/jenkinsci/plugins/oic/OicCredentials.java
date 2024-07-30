jenkinsci/oic-auth-plugin

commit bba32e0929c49244a5d7b42b7b52f3e1f7fa4d3a

Fix redirect loop when oic credentials have expired but jenkins session is still valid (#357)
* clock skew should add extra buffer time, not remove it
* id oicSession has expired, also expire jenkins websession to prevent redirect loops

src/main/java/org/jenkinsci/plugins/oic/OicCredentials.java
@@ -48,7 +48,7 @@ public class OicCredentials extends UserProperty implements Serializable {
 
         if (expiresInSeconds != null && expiresInSeconds > 0) {
             long allowedClockSkewFixed = Util.fixNull(allowedClockSkewSeconds, 60L);
-            this.expiresAtMillis = currentTimestamp + (expiresInSeconds - allowedClockSkewFixed) * 1000;
+            this.expiresAtMillis = (currentTimestamp + expiresInSeconds + allowedClockSkewFixed) * 1000;
         } else {
             this.expiresAtMillis = null;
         }

src/main/java/org/jenkinsci/plugins/oic/OicSecurityRealm.java
@@ -115,7 +115,6 @@ import org.kohsuke.stapler.HttpResponses;
 import org.kohsuke.stapler.QueryParameter;
 import org.kohsuke.stapler.StaplerRequest;
 import org.kohsuke.stapler.StaplerResponse;
-import org.kohsuke.stapler.WebApp;
 import org.kohsuke.stapler.interceptor.RequirePOST;
 import org.springframework.security.authentication.AnonymousAuthenticationToken;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -1420,7 +1419,8 @@ public class OicSecurityRealm extends SecurityRealm implements Serializable {
     private void redirectOrRejectRequest(HttpServletRequest req, HttpServletResponse res)
             throws IOException, ServletException {
         if (req.getSession(false) != null || Strings.isNullOrEmpty(req.getHeader("Authorization"))) {
-            WebApp.get(Jenkins.get().servletContext).getSomeStapler().invoke(req, res, Jenkins.get(), getLoginUrl());
+            req.getSession().invalidate();
+            res.sendRedirect(Jenkins.get().getSecurityRealm().getLoginUrl());
         } else {
             res.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Token expired");
         }

src/test/java/org/jenkinsci/plugins/oic/PluginTest.java
@@ -16,6 +16,10 @@ import edu.umd.cs.findbugs.annotations.Nullable;
 import hudson.model.User;
 import hudson.tasks.Mailer;
 import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 import java.security.KeyPair;
 import java.security.KeyPairGenerator;
 import java.security.MessageDigest;
@@ -34,6 +38,7 @@ import java.util.regex.Pattern;
 import javax.servlet.http.HttpSession;
 import jenkins.model.Jenkins;
 import jenkins.security.LastGrantedAuthoritiesProperty;
+import org.hamcrest.MatcherAssert;
 import org.junit.Before;
 import org.junit.Ignore;
 import org.junit.Rule;
@@ -61,6 +66,7 @@ import static com.github.tomakehurst.wiremock.client.WireMock.verify;
 import static com.google.gson.JsonParser.parseString;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.empty;
+import static org.hamcrest.Matchers.is;
 import static org.jenkinsci.plugins.oic.TestRealm.AUTO_CONFIG_FIELD;
 import static org.jenkinsci.plugins.oic.TestRealm.EMAIL_FIELD;
 import static org.jenkinsci.plugins.oic.TestRealm.FULL_NAME_FIELD;
@@ -343,6 +349,22 @@ public class PluginTest {
         verify(postRequestedFor(urlPathEqualTo("/token")).withRequestBody(containing("grant_type=refresh_token")));
     }
 
+    private HttpResponse<String> getPageWithGet(String url) throws IOException, InterruptedException {
+        // fix up the url, if needed
+        if (url.startsWith("/")) {
+            url = url.substring(1);
+        }
+
+        HttpClient c = HttpClient.newBuilder()
+                .followRedirects(HttpClient.Redirect.NEVER)
+                .build();
+        return c.send(
+                HttpRequest.newBuilder(URI.create(jenkinsRule.getURL() + url))
+                        .GET()
+                        .build(),
+                HttpResponse.BodyHandlers.ofString());
+    }
+
     @Test
     public void testRefreshTokenAndTokenExpiration_withoutRefreshToken() throws Exception {
         mockAuthorizationRedirectsToFinishLogin();
@@ -353,11 +375,11 @@ public class PluginTest {
         mockUserInfoWithTestGroups();
         browseLoginPage();
         assertTestUser();
-
         // expired token not refreshed
         expire();
-        webClient.assertFails(jenkins.getSecurityRealm().getLoginUrl(), 302);
-
+        // use an actual HttpClient to make checking redirects easier
+        HttpResponse<String> rsp = getPageWithGet("/manage");
+        MatcherAssert.assertThat("response should have been 302\n" + rsp.body(), rsp.statusCode(), is(302));
         verify(postRequestedFor(urlPathEqualTo("/token")).withRequestBody(notMatching(".*grant_type=refresh_token.*")));
     }
 
@@ -450,12 +472,13 @@ public class PluginTest {
             User user = User.get2(authentication);
             OicCredentials credentials = user.getProperty(OicCredentials.class);
 
+            // setting currentTimestamp == 1 guarantees this will be an expired cred
             user.addProperty(new OicCredentials(
                     credentials.getAccessToken(),
                     credentials.getIdToken(),
                     credentials.getRefreshToken(),
                     60L,
-                    Clock.SYSTEM.currentTimeMillis() - 3600,
+                    1L,
                     60L));
 
             return null;

commit 64d780ce4c69990078e4d44bba13002492b87255

Fix calculation of expiration timestamp
`currentTimestamp` is in millis already which is why it should not be mulitplied by 1000.

This bug was introduced in bba32e09.

src/main/java/org/jenkinsci/plugins/oic/OicCredentials.java
@@ -48,7 +48,7 @@ public class OicCredentials extends UserProperty implements Serializable {
 
         if (expiresInSeconds != null && expiresInSeconds > 0) {
             long allowedClockSkewFixed = Util.fixNull(allowedClockSkewSeconds, 60L);
-            this.expiresAtMillis = (currentTimestamp + expiresInSeconds + allowedClockSkewFixed) * 1000;
+            this.expiresAtMillis = currentTimestamp + (expiresInSeconds + allowedClockSkewFixed) * 1000;
         } else {
             this.expiresAtMillis = null;
         }