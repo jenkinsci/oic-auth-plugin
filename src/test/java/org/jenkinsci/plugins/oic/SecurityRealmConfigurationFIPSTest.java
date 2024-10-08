48499d3f9e467d4a5275850e5791920e384038c3
Olivier Lamy <olamy@apache.org>
2024-10-08 20:57:13 +1000

    It looks I forgot to add the test java source file...
    Signed-off-by: Olivier Lamy <olamy@apache.org>

---
+++ src/test/java/org/jenkinsci/plugins/oic/SecurityRealmConfigurationFIPSTest.java
@@ -0,0 +1,26 @@
+package org.jenkinsci.plugins.oic;
+
+import org.junit.ClassRule;
+import org.junit.Test;
+import org.jvnet.hudson.test.FlagRule;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+public class SecurityRealmConfigurationFIPSTest {
+
+    @ClassRule
+    public static FlagRule<String> FIPS_RULE = FlagRule.systemProperty("jenkins.security.FIPS140.COMPLIANCE", "true");
+
+    @Test(expected = IllegalArgumentException.class)
+    public void escapeHatchThrowsException() throws Exception {
+        new OicSecurityRealm("clientId", null, null, null).setEscapeHatchEnabled(true);
+    }
+
+    @Test
+    public void escapeHatchToFalse() throws Exception {
+        OicSecurityRealm oicSecurityRealm = new OicSecurityRealm("clientId", null, null, null);
+        oicSecurityRealm.setEscapeHatchEnabled(false);
+        assertThat(oicSecurityRealm.isEscapeHatchEnabled(), is(false));
+    }
+}
