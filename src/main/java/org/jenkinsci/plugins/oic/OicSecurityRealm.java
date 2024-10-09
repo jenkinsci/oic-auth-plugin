462376875ea244608d548e435fcd4e654a6a05be
Olivier Lamy <olamy@apache.org>
2024-10-09 18:10:10 +1000

    use etEscapeHatchEnabled validation in readresolve
    Signed-off-by: Olivier Lamy <olamy@apache.org>

--- src/main/java/org/jenkinsci/plugins/oic/OicSecurityRealm.java
+++ src/main/java/org/jenkinsci/plugins/oic/OicSecurityRealm.java
@@ -321,6 +321,7 @@ public class OicSecurityRealm extends SecurityRealm implements Serializable {
         // ensure escapeHatchSecret is encrypted
         this.setEscapeHatchSecret(this.escapeHatchSecret);
         try {
+            this.setEscapeHatchEnabled(this.escapeHatchEnabled);
             if (automanualconfigure != null) {
                 if ("auto".equals(automanualconfigure)) {
                     OicServerWellKnownConfiguration conf =
