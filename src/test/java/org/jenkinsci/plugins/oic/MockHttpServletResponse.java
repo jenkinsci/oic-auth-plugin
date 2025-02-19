f4e00636e882cc2f286f278c89b0e5c559321728
Eva Müller <emueller@coremedia.com>
2025-02-19 08:22:49 +0100

    Extract logic from handleTokenExpiration method to dedicated method
    Also add tests per extracted method

---
+++ src/test/java/org/jenkinsci/plugins/oic/MockHttpServletResponse.java
@@ -0,0 +1,199 @@
+package org.jenkinsci.plugins.oic;
+
+import jakarta.servlet.ServletOutputStream;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Collection;
+import java.util.Locale;
+
+public class MockHttpServletResponse implements HttpServletResponse {
+
+    public MockHttpServletResponse() {}
+
+    @Override
+    public void addCookie(Cookie cookie) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public boolean containsHeader(String s) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public String encodeURL(String s) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public String encodeRedirectURL(String s) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public String encodeUrl(String s) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public String encodeRedirectUrl(String s) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void sendError(int i, String s) throws IOException {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void sendError(int i) throws IOException {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void sendRedirect(String s) throws IOException {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void setDateHeader(String s, long l) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void addDateHeader(String s, long l) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void setHeader(String s, String s1) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void addHeader(String s, String s1) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void setIntHeader(String s, int i) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void addIntHeader(String s, int i) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void setStatus(int i) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void setStatus(int i, String s) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public int getStatus() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public String getHeader(String s) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public Collection<String> getHeaders(String s) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public Collection<String> getHeaderNames() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public String getCharacterEncoding() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public String getContentType() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public ServletOutputStream getOutputStream() throws IOException {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public PrintWriter getWriter() throws IOException {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void setCharacterEncoding(String s) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void setContentLength(int i) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void setContentLengthLong(long l) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void setContentType(String s) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void setBufferSize(int i) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public int getBufferSize() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void flushBuffer() throws IOException {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void resetBuffer() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public boolean isCommitted() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void reset() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void setLocale(Locale locale) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public Locale getLocale() {
+        throw new UnsupportedOperationException();
+    }
+}
--- src/test/java/org/jenkinsci/plugins/oic/OicSecurityRealmTest.java
+++ src/test/java/org/jenkinsci/plugins/oic/OicSecurityRealmTest.java
@@ -1,7 +1,10 @@
 package org.jenkinsci.plugins.oic;
 
 import com.github.tomakehurst.wiremock.junit5.WireMockExtension;
+import hudson.model.User;
+import hudson.security.SecurityRealm;
 import hudson.util.Secret;
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 import org.junit.jupiter.api.Test;
@@ -12,8 +15,10 @@ import org.springframework.security.authentication.AnonymousAuthenticationToken;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.bcrypt.BCrypt;
 
 import static com.github.tomakehurst.wiremock.core.WireMockConfiguration.wireMockConfig;
@@ -21,6 +26,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
@@ -141,5 +147,89 @@ class OicSecurityRealmTest {
         assertTrue(realm.doCheckEscapeHatch(escapeHatchUsername, escapeHatchPassword));
         assertFalse(realm.doCheckEscapeHatch("otherUsername", escapeHatchPassword));
         assertFalse(realm.doCheckEscapeHatch(escapeHatchUsername, "wrongPassword"));
+    }
+
+    @Test
+    public void testHandleTokenExpiration_logoutRequestUri() throws Exception {
+        TestRealm realm = new TestRealm.Builder(wireMock).WithMinimalDefaults().build();
+
+        MockHttpServletRequest request = new MockHttpServletRequest() {
+            @Override
+            public String getRequestURI() {
+                return "/logout";
+            }
+        };
+        MockHttpServletResponse response = new MockHttpServletResponse();
+
+        assertTrue(realm.isLogoutRequest(request));
+        assertTrue(realm.handleTokenExpiration(request, response));
+    }
+
+    @Test
+    public void testHandleTokenExpiration_noAuthenticationOrAnonymous() throws Exception {
+        TestRealm realm = new TestRealm.Builder(wireMock).WithMinimalDefaults().build();
+
+        MockHttpServletRequest request = new MockHttpServletRequest();
+        MockHttpServletResponse response = new MockHttpServletResponse();
+
+        SecurityContextHolder.getContext().setAuthentication(null);
+        assertFalse(realm.isLogoutRequest(request));
+        assertTrue(realm.isNotAuthenticatedOrAnonymous(null));
+        assertTrue(realm.handleTokenExpiration(request, response));
+
+        String key = "testKey";
+        Object principal = "testUser";
+
+        List<org.springframework.security.core.GrantedAuthority> grantedAuthorities = new ArrayList<>();
+        grantedAuthorities.add(SecurityRealm.AUTHENTICATED_AUTHORITY2);
+        org.springframework.security.authentication.AnonymousAuthenticationToken token =
+                new org.springframework.security.authentication.AnonymousAuthenticationToken(
+                        key, principal, grantedAuthorities);
+        SecurityContextHolder.getContext().setAuthentication(token);
+
+        assertFalse(realm.isLogoutRequest(request));
+        assertTrue(realm.isNotAuthenticatedOrAnonymous(token));
+        assertTrue(realm.handleTokenExpiration(request, response));
+    }
+
+    @Test
+    public void testHandleTokenExpiration_noOicCredentials(JenkinsRule jenkinsRule) throws Exception {
+        TestRealm realm = new TestRealm.Builder(wireMock).WithMinimalDefaults().build();
+
+        MockHttpServletRequest request = new MockHttpServletRequest();
+        MockHttpServletResponse response = new MockHttpServletResponse();
+
+        assertFalse(realm.isLogoutRequest(request));
+        Authentication a = SecurityContextHolder.getContext().getAuthentication();
+        assertFalse(realm.isNotAuthenticatedOrAnonymous(a));
+
+        jenkinsRule.jenkins.getUser(a.getName());
+        User user = User.get2(a);
+        assertNotNull(user);
+        assertNull(user.getProperty(OicCredentials.class));
+        assertTrue(realm.handleTokenExpiration(request, response));
+    }
+
+    @Test
+    public void testIsValidApiTokenRequest_NoTokenAccessWithoutOicSession(JenkinsRule jenkinsRule) throws Exception {
+        TestRealm realm = new TestRealm.Builder(wireMock).WithMinimalDefaults().build();
+
+        MockHttpServletRequest request = new MockHttpServletRequest();
+        assertFalse(realm.isLogoutRequest(request));
+
+        List<org.springframework.security.core.GrantedAuthority> grantedAuthorities = new ArrayList<>();
+        grantedAuthorities.add(SecurityRealm.AUTHENTICATED_AUTHORITY2);
+        UsernamePasswordAuthenticationToken token =
+                new UsernamePasswordAuthenticationToken("test-user", "", grantedAuthorities);
+        SecurityContextHolder.getContext().setAuthentication(token);
+
+        assertFalse(realm.isNotAuthenticatedOrAnonymous(token));
+
+        jenkinsRule.jenkins.getUser(token.getName());
+        User user = User.get2(token);
+        assertNotNull(user);
+        user.addProperty(new OicCredentials("test", "test", "test", 1L, 1L, 1L));
+        assertNotNull(user.getProperty(OicCredentials.class));
+        assertFalse(realm.isValidApiTokenRequest(request, user));
     }
 }
