dde57435d1dde4f40250d83313f124de24aa94a5
Eva Müller <emueller@coremedia.com>
2025-03-24 00:28:22 +0100

    Fix typo

--- src/main/java/org/jenkinsci/plugins/oic/AbstractKeyValueDescribable.java
+++ src/main/java/org/jenkinsci/plugins/oic/AbstractKeyValueDescribable.java
@@ -79,7 +79,7 @@ public abstract class AbstractKeyValueDescribable<T extends AbstractKeyValueDesc
         /**
          * Check the key for validity.
          * In addition to being used by the UI, any FormValidation of {@code Kind.ERROR} will cause a fail to create the describable.
-         * By default this method returns {@link FormValidation#ok()}, subclasses should override this in order to provide any required checking.
+         * By default, this method returns {@link FormValidation#ok()}, subclasses should override this in order to provide any required checking.
          */
         public FormValidation doCheckValue(@SuppressWarnings("unused") @QueryParameter String value) {
             return FormValidation.ok();
