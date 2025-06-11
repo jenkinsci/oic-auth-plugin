bcba3acbc0870915f5f359f011c37431db1532fa
Eva Müller <emueller@coremedia.com>
2025-06-11 08:22:22 +0200

    Remove unnecessary return statement in 'void' method

--- src/main/java/org/jenkinsci/plugins/oic/OicSecurityRealm.java
+++ src/main/java/org/jenkinsci/plugins/oic/OicSecurityRealm.java
@@ -1034,7 +1034,6 @@ public class OicSecurityRealm extends SecurityRealm implements Serializable {
         // store the redirect url for after the login.
         sessionStore.set(webContext, SESSION_POST_LOGIN_REDIRECT_URL_KEY, redirectOnFinish);
         JEEHttpActionAdapter.INSTANCE.adapt(redirectionAction, webContext);
-        return;
     }
 
     private void randomWait() {
@@ -1427,7 +1426,6 @@ public class OicSecurityRealm extends SecurityRealm implements Serializable {
         } catch (HttpAction e) {
             // this may be an OK flow for logout login is handled upstream.
             JEEHttpActionAdapter.INSTANCE.adapt(e, webContext);
-            return;
         }
     }
 
